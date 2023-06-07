da9b4a46f267ba58a4017929213f26fffe33aeed
Federico Dacal <federicodacal@gmail.com>
2023-06-07 12:50:00 -0300

    creacion de entorno y firebase

---
+++ sala_de_juegos/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'sala-2f32c',
+    appId: '1:1095695716404:web:24b65e32dfbd771a9a3a61',
+    storageBucket: 'sala-2f32c.appspot.com',
+    apiKey: 'AIzaSyAgOdENapm6UuFIYmhh_nVfVWcbMnKLZH4',
+    authDomain: 'sala-2f32c.firebaseapp.com',
+    messagingSenderId: '1095695716404',
+  },};
